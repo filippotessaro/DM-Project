ef959c099eb047ee471059181e8bdff3b2c1da5e
filippotessaro <filippotessaro96@gmail.com>
2020-01-29 20:26:56 +0100

    Fixed Item 2 vector to work with groceries.csv

--- Utils/item2Vector.py
+++ Utils/item2Vector.py
@@ -1,45 +1,48 @@
 """
 This file is useful to convert a file with products as string
 into a file with IDs encoded in a correct way.
+
+OUTPUT:
+- dictionary file ../outData/norm-dict.txt
+- vectorized dataset ../outData/norm-groceries.txt
 """
 
-superm = []
 baskets = []
 with open('../data/groceries.csv', 'r') as f:
     for line in f:
         basket = []
+        line = line[:-1]
         items = line.split(",")
         for item in items:
-            superm.append(item)
             basket.append(item)
         baskets.append(basket)
 
+#baskets = set(baskets)
 elems = set()
 for b in baskets:
     elems.update(b)
 
+
 mapping = {}
 reverseMapping = {}
 key = 0
-for e in elems:
-    mapping[e] = key
-    reverseMapping[key] = e
+for basket in elems:
+    mapping[basket] = key
+    reverseMapping[key] = basket
     key += 1
 
 print("Mapping")
 print(mapping)
 
-f = open("../outData/dict.txt","w")
-f.write( str(mapping) )
-f.close()
+dict = open("../outData/norm-dict.txt", "w")
+dict.write(str(mapping))
+dict.close()
 
-newBaskets = []
-
-fileNew = open('../outData/test.txt', 'a')
-
+ds = open('../outData/norm-groceries.txt', 'a')
 for basket in baskets:
     line_file = list()
     for item in basket:
         line_file.append(mapping[item])
-    fileNew.write(str(line_file) + '\n')
+    ds.write(str(line_file)[1:-1] + '\n')
     line_file = []
+ds.close()
